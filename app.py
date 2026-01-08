8e7093d2bd5e2995314afe49db68f332188444a5
Fábio Galdino <fabio.galdino989@gmail.com>
2026-01-08 18:18:40 -0300

    Segundo Teste - Processo Seletivo
    * Importação de dados finalizada
    
    * Atualizando .gitignore
    
    * Removendo pasta desnecessária
    
    * Relatório executivo finalizado

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ def create_app(config_name = 'development'):
         
     @app.route('/')
     def index():
+        return render_template('relatorio.html')
+
+    @app.route('/dashboard')
+    def dashboard():
         return render_template('dashboard.html')
     
 
